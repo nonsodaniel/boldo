c6f8c8b900a8d5e03614ebe5d6b7f6684dbcd75b
nonsodaniel <nonsodaniel07@gmail.com>
2024-01-17 13:47:46 +0100

    Improve Navigation UX

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+"use client";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   Welcome to my world
+    <main className="flex min-h-screen flex-col items-center justify-center  p-24">
+      <h1 className="text-black font-extrabold text-3xl pb-5">
+        Welcome to Boldo! Click the link below to view the Blog I just created.
+      </h1>
+      <Link href={"/blog"} className="text-blue-500 hover:text-blue-700">
+        Go to Blog &rarr;
+      </Link>
     </main>
-  )
+  );
 }
